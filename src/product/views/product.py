ba503c748e9127cfc771aa3e98a6248137941885
bappi-repliq <abobashar.bappi@repliq.dev>
2024-06-10 02:55:36 +0600

    products list and added filters

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,14 +1,85 @@
 from django.views import generic
 
 from product.models import Variant
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from product.models import Variant, Product, ProductVariant, ProductVariantPrice
+
+from django.db.models import Subquery, OuterRef, Q, Max, Prefetch
 
 
 class CreateProductView(generic.TemplateView):
-    template_name = 'products/create.html'
+    template_name = "products/create.html"
 
     def get_context_data(self, **kwargs):
         context = super(CreateProductView, self).get_context_data(**kwargs)
+        variants = Variant.objects.filter(active=True).values("id", "title")
+        context["product"] = True
+        context["variants"] = list(variants.all())
+        return context
+
+
+"""
+Products list
+"""
+
+
+class ProductsList(generic.TemplateView):
+    template_name = "products/list.html"
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        products = Product.objects.all()
+
+        title = self.request.GET.get("title")
+        variant = self.request.GET.getlist("variant")
+        price_from = self.request.GET.get("price_from")
+        price_to = self.request.GET.get("price_to")
+        date = self.request.GET.get("date")
+
+        if title:
+            products = products.filter(title__icontains=title)
+
+        if variant:
+            filtered_variants = ProductVariant.objects.filter(
+                variant_title__in=variant,
+                product=OuterRef("pk"),
+            ).values("product")
+            products = products.filter(Q(pk__in=Subquery(filtered_variants)))
+
+        if price_from and price_to:
+            filtered_variants = ProductVariantPrice.objects.filter(
+                price__gte=price_from,
+                price__lte=price_to,
+                product=OuterRef("pk"),
+            ).values("product")
+            products = products.filter(Q(pk__in=Subquery(filtered_variants)))
+
+        if date:
+            products = products.filter(created_at__date=date)
+
+        page = self.request.GET.get("page")
+        paginator = Paginator(products, self.paginate_by)
+        try:
+            products = paginator.page(page)
+        except PageNotAnInteger:
+            products = paginator.page(1)
+        except EmptyPage:
+            products = paginator.page(paginator.num_pages)
+
+        context["product"] = True
+        context["products"] = products
+        context["paginator"] = paginator
+
         variants = Variant.objects.filter(active=True).values('id', 'title')
-        context['product'] = True
-        context['variants'] = list(variants.all())
+
+        for variant in variants:
+            variant['variants'] = (
+                ProductVariant.objects.filter(variant_id=variant['id'])
+                .values('variant_title')
+                .distinct()
+            )
+
+        context['variants'] = variants
+        context['selected_variants'] = self.request.GET.getlist('variant')
         return context
